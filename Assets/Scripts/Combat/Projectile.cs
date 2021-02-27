31e379b58cfda40e4710018557f2b73f23bb7019
Kyle Weeks <60368775+kyleweeks88@users.noreply.github.com>
2021-02-26 21:36:57 -0800

    more stuff

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -15,7 +15,7 @@ public class Projectile : NetworkBehaviour
 		StartCoroutine(DestroyAfterLifetime());
     }
 
- //   [ServerCallback]
+	//   [ServerCallback]
 	//void Update()
 	//{
 	//	if (speed > 0)
@@ -27,6 +27,12 @@ public class Projectile : NetworkBehaviour
 	//	}
 	//}
 
+	public void SetSpeed(float newSpeed)
+	{
+		speed = newSpeed;
+		StartCoroutine(SpawnProjectiles(newSpeed));
+	}
+
 	IEnumerator SpawnProjectiles(float speed)
     {
 		Vector3 direction = transform.forward;
@@ -35,32 +41,86 @@ public class Projectile : NetworkBehaviour
 		while(this.gameObject != null)
         {
 			this.transform.position += (direction * speed * Time.deltaTime);
-			CheckCollisions(0);
+			CheckCollisions();
 			yield return wait;
         }
     }
 
-	public void SetSpeed(float newSpeed)
-    {
-        speed = newSpeed;
-		StartCoroutine(SpawnProjectiles(newSpeed));
-    }
-
-	void CheckCollisions(float moveDistance)
+	void CheckCollisions()
 	{
 		Ray ray = new Ray(transform.position, transform.forward);
 		RaycastHit hit;
 
-		if (Physics.Raycast(ray, out hit, moveDistance + skinWidth, collisionsMask, QueryTriggerInteraction.Collide))
+		if (Physics.Raycast(ray, out hit, skinWidth, collisionsMask, QueryTriggerInteraction.Collide))
 		{
+			// Instantiate HitFX here
 			OnHitObject(hit);
-			Debug.Log("HIT");
+		}
+
+		CmdCheckCollisions();
+	}
+
+	[Command]
+	void CmdCheckCollisions()
+    {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, skinWidth, collisionsMask, QueryTriggerInteraction.Collide))
+		{
+			// Instantiate HitFX here
+			OnHitObject(hit);
+		}
+
+		RpcCheckCollisions();
+	}
+
+	[ClientRpc]
+	void RpcCheckCollisions()
+    {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, skinWidth, collisionsMask, QueryTriggerInteraction.Collide))
+		{
+			// Instantiate HitFX here
+			OnHitObject(hit);
 		}
 	}
 
 	void OnHitObject(RaycastHit hit)
 	{
 		CharacterStats hitTarget = hit.collider.GetComponent<CharacterStats>();
+		if (hitTarget != null)
+		{
+			hitTarget.TakeDamage(projectileDamage);
+		}
+
+		NetworkIdentity _hit = hit.collider.gameObject.GetComponent<NetworkIdentity>();
+		CmdOnHitObject(_hit);
+		GameObject.Destroy(gameObject);
+		Debug.Log("HIT");
+	}
+
+	[Command]
+	void CmdOnHitObject(NetworkIdentity hit)
+    {
+		CharacterStats hitTarget = hit.gameObject.GetComponent<CharacterStats>();
+		if (hitTarget != null)
+		{
+			hitTarget.TakeDamage(projectileDamage);
+		}
+
+		RpcOnHitObject(hit);
+		GameObject.Destroy(gameObject);
+	}
+
+	[ClientRpc]
+	void RpcOnHitObject(NetworkIdentity hit)
+    {
+        if (base.hasAuthority) { return; }
+
+		CharacterStats hitTarget = hit.gameObject.GetComponent<CharacterStats>();
 		if (hitTarget != null)
 		{
 			hitTarget.TakeDamage(projectileDamage);
